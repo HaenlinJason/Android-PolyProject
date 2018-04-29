e9a67b660cbad8f9f323bab8a5df5cd33502426f
Jason Haenlin <jason.Haenlin@etu.unice.fr>
2018-04-29 16:51:14 +0200

    ajoute des permissions et la classe MyStorage (in progress)

--- app/src/main/java/com/incident/polyandroid/MainActivity.java
+++ app/src/main/java/com/incident/polyandroid/MainActivity.java
@@ -15,6 +15,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.incident.polyandroid.firebase.MyStorage;
 import com.incident.polyandroid.fragment.MyEventFragment;
 
 public class MainActivity extends AppCompatActivity
@@ -26,25 +27,26 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -55,11 +57,14 @@ public class MainActivity extends AppCompatActivity
         fragmentTransaction.add(R.id.main_content_fragment, fragment);
         fragmentTransaction.commit();
 
+        MyStorage st = new MyStorage();
+        st.uploadFile("/storage/emulated/0/DCIM/Camera/IMG_20180429_090121.jpg");
+
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -109,7 +114,7 @@ public class MainActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
--- app/src/main/java/com/incident/polyandroid/firebase/MyStorage.java
+++ app/src/main/java/com/incident/polyandroid/firebase/MyStorage.java
@@ -1,4 +1,45 @@
 package com.incident.polyandroid.firebase;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+
 public class MyStorage {
+
+    private final static String TAG = "DEBUG_STORAGE";
+
+    private StorageReference mStorageRef;
+
+    public MyStorage() {
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+    }
+
+    public void uploadFile(String path) {
+        Uri file = Uri.fromFile(new File(path));
+        StorageReference riversRef = mStorageRef.child("images/" + file.getLastPathSegment());
+
+        riversRef.putFile(file)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Log.d(TAG, "url : " + downloadUrl);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Log.d(TAG, "error while uploading the file", exception);
+                    }
+                });
+    }
 }
